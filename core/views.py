58be220599ceceaff1c285ab4b8e7399243bdd34
Ariston <aristoncatipay123@gmail.com>
2023-10-20 01:06:40 +0800

    Created the signup functionality.

--- core/views.py
+++ core/views.py
@@ -29,6 +29,34 @@ def signin(request):
     })
 
 def signup(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email is already taken.')
+                return redirect('core:signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is already taken.')
+                return redirect('core:signup')
+            else:
+                # Create the user
+                user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+                user.save()
+                # Log the user in using the credentials
+                credentials = auth.authenticate(username=username, password=password)
+                auth.login(request, credentials)
+
+                return redirect('core:index')
+        else:
+            messages.info(request, 'Password don\'t match')
+            return redirect('core:signup')
+
     return render(request, 'core/signup.html', {
         'title': 'Sign up',
     })
