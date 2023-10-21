34d39935d0456683a677add02ec103dffbf0310a
Ariston <aristoncatipay123@gmail.com>
2023-10-21 19:40:09 +0800

    Created the updated profile functionality.

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from . models import Profile
 
@@ -16,6 +16,51 @@ def index(request):
 def edit(request):
     user = User.objects.get(username=request.user.username)
     profile = Profile.objects.get(user=user)
+
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        student_number = request.POST['student_number'] 
+        student_contact_no = request.POST['student_contact_no']
+
+        if request.FILES.get('image') == None:
+            # If the user didn't upload their own image
+            # Use the default profile image.
+
+            image = profile.image
+
+            # Update profile model
+            profile.image = image
+            profile.student_number = student_number
+            profile.student_contact_no = student_contact_no
+            profile.save()
+
+            # Updated the user model 
+            user.first_name = first_name
+            user.last_name = last_name
+            user.username = username
+            user.email = email
+            user.save()
+
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+
+            # Update profile model
+            profile.image = image
+            profile.student_number = student_number
+            profile.student_contact_no = student_contact_no
+            profile.save()
+
+            # Updated the user model 
+            user.first_name = first_name
+            user.last_name = last_name
+            user.username = username
+            user.email = email
+            user.save()
+
+        return redirect('profile:edit')
 
     return render(request, 'user_profile/edit.html', {
         'title': 'Edit Profile',
