c9db6fc3586cfe2fb0314360a0ec4469314c2e58
Ariston <aristoncatipay123@gmail.com>
2023-10-24 13:34:53 +0800

    Created the book model and the default photos used for each book.

--- book/models.py
+++ book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    cover_image = models.ImageField(upload_to='book_images/cover', default='default_book_image.jpg')
+    overview_image = models.ImageField(upload_to='book_images/overview', default='default_book_overview_image.jpg')
+    table_of_contents_image = models.ImageField(upload_to='book_images/table_of_contents', default='default_book_table_of_contents_image.jpg')
+    isbn_number = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    date_published = models.DateField(null=True)
+    inventory = models.IntegerField()
+    rack_number = models.CharField(max_length=255)
+    rack_level_number = models.CharField(max_length=255)
