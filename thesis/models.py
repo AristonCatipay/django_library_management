f8506e3858af2fd23c0292f555251309f424b3d7
Ariston <aristoncatipay123@gmail.com>
2023-10-25 21:57:41 +0800

    Created the thesis model.

--- thesis/models.py
+++ thesis/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from course.models import Course
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='thesis_author_images', default='default_profile_image.jpg')
+
+    def __str__(self):
+        return self.name
+
+class Thesis(models.Model):
+    title = models.CharField(max_length=255)
+    date_published = models.DateField(null=True)
+    course = models.ForeignKey(Course, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+class AuthorList(models.Model):
+    thesis = models.ForeignKey(Thesis, related_name='thesis_id', on_delete=models.PROTECT)
+    author = models.ForeignKey(Author, related_name='author_id', on_delete=models.PROTECT)
+
+
+    
+
+    
