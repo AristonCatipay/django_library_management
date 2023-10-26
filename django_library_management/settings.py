4c4944ee5e5adfd094511a29f23d2a019d782a3d
Ariston <aristoncatipay123@gmail.com>
2023-10-26 22:47:51 +0800

    Created the suggestion model and register the suggestion app to the installed apps

--- django_library_management/settings.py
+++ django_library_management/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'course',
     'book',
     'thesis',
+    'suggestion',
 ]
 
 MIDDLEWARE = [
--- suggestion/models.py
+++ suggestion/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Suggestion(models.Model):
+    book_title = models.CharField(max_length=255)
+    comment = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
