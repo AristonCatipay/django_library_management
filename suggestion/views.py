cdb4b8b9ebd694093859ef4b3def6f12b6028d44
Ariston <aristoncatipay123@gmail.com>
2023-10-27 13:31:01 +0800

    Displayed the users suggestions in the suggestion index page.

--- suggestion/views.py
+++ suggestion/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from user_profile.models import Profile
+from .models import Suggestion
 
 def index(request):
     # Get user profile.
     user = User.objects.get(username=request.user.username)
     profile = Profile.objects.get(user=user)
+
+    suggestions = Suggestion.objects.all()
     return render(request, 'suggestion/index.html', {
         'title': 'Suggestion',
         'profile': profile,
+        'suggestions': suggestions,
     })
