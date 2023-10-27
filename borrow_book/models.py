de0db78fc4e9ccb9ab80a6247cd6e25a6b6f5421
Ariston <aristoncatipay123@gmail.com>
2023-10-27 19:26:32 +0800

    Created the borrow book model

--- borrow_book/models.py
+++ borrow_book/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from book.models import Book
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Borrow_Book(models.Model):
+    REQUEST = 'Request' 
+    BORROWED = 'Borrowed'
+    PENDING = 'Pending'
+    APPROVED = 'Approved'
+
+    REQUEST_STATUS_CHOICES = {
+        (REQUEST, 'Request'),
+        (BORROWED, 'Borrowed'),
+        (PENDING, 'Pending'),
+        (APPROVED, 'Approved')
+    }
+
+    request_status = models.CharField(max_length=15, choices=REQUEST_STATUS_CHOICES, default=REQUEST)
+    request_created = models.DateTimeField(auto_now_add=True)
+    pick_up_date = models.DateField()
+    return_due_date = models.DateField()
+    return_date = models.DateField()
+    pending_days = models.IntegerField()
+    fine = models.IntegerField()
+    staff_approve = models.ForeignKey(User, related_name='staff_approve_id', on_delete=models.PROTECT)
+    staff_borrow = models.ForeignKey(User, related_name='staff_borrow_id', on_delete=models.PROTECT)
+    staff_return = models.ForeignKey(User, related_name='staff_return_id', on_delete=models.PROTECT)
+
+
