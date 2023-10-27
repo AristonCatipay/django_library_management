ac20dd001feb98aa019edb334926a629950881ef
Ariston <aristoncatipay123@gmail.com>
2023-10-27 19:37:13 +0800

    Updated the URLs of books and thesis, followed the pep-8 recommendation. Added the foriegn key book in the borrow book model.

--- book/urls.py
+++ book/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('add/', views.add, name='add'),
     path('detail/<int:primary_key>/', views.detail, name='detail'),
     path('edit/<int:primary_key>/', views.edit, name='edit'),
-    path('add_author/', views.add_author, name='add_author'),
-    path('add_author_in_list/<int:primary_key>/', views.add_author_in_author_list, name='add_author_in_author_list'),
+    path('author/add/', views.add_author, name='add_author'),
+    path('author-in-list/add/<int:primary_key>/', views.add_author_in_author_list, name='add_author_in_author_list'),
 ]
--- borrow_book/models.py
+++ borrow_book/models.py
@@ -14,7 +14,7 @@ class Borrow_Book(models.Model):
         (PENDING, 'Pending'),
         (APPROVED, 'Approved')
     }
-
+    
     request_status = models.CharField(max_length=15, choices=REQUEST_STATUS_CHOICES, default=REQUEST)
     request_created = models.DateTimeField(auto_now_add=True)
     pick_up_date = models.DateField()
@@ -22,6 +22,7 @@ class Borrow_Book(models.Model):
     return_date = models.DateField()
     pending_days = models.IntegerField()
     fine = models.IntegerField()
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
     staff_approve = models.ForeignKey(User, related_name='staff_approve_id', on_delete=models.PROTECT)
     staff_borrow = models.ForeignKey(User, related_name='staff_borrow_id', on_delete=models.PROTECT)
     staff_return = models.ForeignKey(User, related_name='staff_return_id', on_delete=models.PROTECT)
