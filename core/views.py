0df124a6264d53353ac8b79e56defe9b5e93cf4e
Ariston <aristoncatipay123@gmail.com>
2023-10-31 23:23:15 +0800

    Displayed the statistics data into the dashboard.

--- core/views.py
+++ core/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, redirect
 from user_profile.models import Profile
 from course.models import Course
 from .decorators import unauthenticated_user
+from book.models import Book
+from thesis.models import Thesis
 # Create your views here.
 
 @login_required
@@ -13,10 +15,21 @@ def index(request):
     profile = Profile.objects.get(user=user)
     is_staff = True if user.groups.filter(name='staff') else False
 
+    # Statistics
+    books_total = Book.objects.count()
+    thesis_total = Thesis.objects.count()
+    students_total = User.objects.filter(groups__name='student').count()
+    teachers_total = User.objects.filter(groups__name='teacher').count()
+
     return render(request, 'core/index.html', {
         'title': 'Welcome',
         'profile': profile,
         'is_staff': is_staff,
+        'books_total': books_total,
+        'thesis_total': thesis_total,
+        'students_total': students_total,
+        'teachers_total': teachers_total,
+
     })
 
 @unauthenticated_user
