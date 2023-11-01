8d358eeddda7f6880fbe6b87c3b5e506d31350d4
Ariston <aristoncatipay123@gmail.com>
2023-11-01 20:23:51 +0800

    Created the review and revieweditem models.

--- review/models.py
+++ review/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+class Review(models.Model):
+    review = models.CharField(max_length=255)
+
+class ReviewedItem(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # Type: Books, Thesis
+    object_id = models.PositiveIntegerField()
+    # ID of that object
+    # Assuming every table has a primary key and all primary key are positive.
+    content_object = GenericForeignKey()
+    # Getting the actual object.
