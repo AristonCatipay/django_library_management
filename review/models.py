7fd8f1ed01e3f5ac89304dbadbabc671ae1755ff
Ariston <aristoncatipay123@gmail.com>
2023-11-03 16:01:22 +0800

    Updated the name of revieweditem model to reviewed_item. Add returned to the request_status in borrow book model.

--- review/models.py
+++ review/models.py
@@ -5,7 +5,7 @@ from django.contrib.contenttypes.models import ContentType
 class Review(models.Model):
     review = models.CharField(max_length=255)
 
-class ReviewedItem(models.Model):
+class Reviewed_Item(models.Model):
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     # Type: Books, Thesis
