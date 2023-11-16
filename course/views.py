402d44d514f870de1ea719901c90759dd01813dc
Ariston <aristoncatipay123@gmail.com>
2023-11-16 19:18:52 +0800

    Remove the parts in all course app view function that retrieves the user profile for the base to display the profile picture.

--- course/views.py
+++ course/views.py
@@ -5,22 +5,17 @@ from .models import Course
 from .forms import CourseForm
 
 def index(request):
-    user = User.objects.get(username=request.user.username)
-    profile = Profile.objects.get(user=user)
-    is_staff = True if user.groups.filter(name='staff') else False
+    is_staff = True if request.user.groups.filter(name='staff') else False
     courses = Course.objects.all()
 
     return render(request, 'course/index.html', {
         'title': 'Course',
-        'profile': profile,
         'is_staff': is_staff,
         'courses': courses,
     })
 
 def add(request):
-    user = User.objects.get(username=request.user.username)
-    profile = Profile.objects.get(user=user)
-    is_staff = True if user.groups.filter(name='staff') else False
+    is_staff = True if request.user.groups.filter(name='staff') else False
 
     if request.method == 'POST':
         form = CourseForm(request.POST)
@@ -31,15 +26,12 @@ def add(request):
         form = CourseForm()
     return render(request, 'course/form.html', {
         'title': 'Add Course',
-        'profile': profile,
         'is_staff': is_staff,
         'form': form,
     })
 
 def edit(request, primary_key):
-    user = User.objects.get(username=request.user.username)
-    profile = Profile.objects.get(user=user)
-    is_staff = True if user.groups.filter(name='staff') else False
+    is_staff = True if request.user.groups.filter(name='staff') else False
 
     course = get_object_or_404(Course, pk=primary_key)
     if request.method == 'POST':
@@ -51,7 +43,6 @@ def edit(request, primary_key):
         form = CourseForm(instance=course)
     return render(request, 'course/form.html', {
         'title': 'Edit Course',
-        'profile': profile,
         'is_staff': is_staff,
         'form': form,
     })
