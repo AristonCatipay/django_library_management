9ef75ba53cef23932e465a2aca3db0d3f50e259d
Ariston <aristoncatipay123@gmail.com>
2023-12-01 20:40:24 +0800

    Create test for home view function.

---
+++ core/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class CoreViewTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+
+    def test_home_view(self):
+        url = reverse('core:home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response ,'core/home.html')
+    
+    def tearDown(self):
+        self.user.delete()
