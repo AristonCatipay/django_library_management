07827ec4c3a91ade9c5b12408dc2aa7821848874
Ariston <aristoncatipay123@gmail.com>
2023-12-01 20:27:39 +0800

    Create test for home page urls.

--- core/tests/test_urls.py
+++ core/tests/test_urls.py
@@ -1,1 +1,9 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from core.views import home, index, signin ,signup, logout, users
+
+class TestUrls(SimpleTestCase):
+    def test_home_url(self):
+        url = reverse('core:home')
+        self.assertEquals(resolve(url).func, home)
 
