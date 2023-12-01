7a3d09bb7aab388265cbd5906b334248227c4208
Ariston <aristoncatipay123@gmail.com>
2023-12-01 18:53:59 +0800

    Create test for edit view function.

--- user_profile/tests/test_views.py
+++ user_profile/tests/test_views.py
@@ -35,6 +35,35 @@ class ProfileViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response ,'user_profile/index.html')
 
+    def test_edit_view(self):
+        self.client.force_login(self.user)
+        url = reverse('profile:edit')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user_profile/edit.html')
+
+        data = {
+            'first_name': self.user.first_name,
+            'last_name': self.user.last_name,
+            'username': self.user.username,
+            'email': self.user.email,
+            'password': self.user.password,
+            'gender': self.profile.gender,
+            'student_number': self.profile.student_number,
+            'student_contact_no': self.profile.student_contact_no,
+            'course' : self.profile.course,
+        }
+        response = self.client.post(url, data)
+        print("\nTest Data Used (Profile edit):", data, "\n")
+
+        if response.context:
+            # Retrieve form instance to access errors
+            form = response.context['form']
+            if form.errors:
+                print(form.errors)
+
+        self.assertEqual(response.status_code, 302)
+    
     def tearDown(self):
         # Cleanup after each test
         self.profile.delete()
