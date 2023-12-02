947b1c85d49b3b0b9ce43f3b13874c7019158f50
Ariston <aristoncatipay123@gmail.com>
2023-12-02 21:50:19 +0800

    Create test for add suggestion page urls.

--- suggestion/tests/test_urls.py
+++ suggestion/tests/test_urls.py
@@ -1,8 +1,12 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from suggestion.views import index
+from suggestion.views import index, add
 
 class SuggestionTestUrls(SimpleTestCase):
     def test_index_url(self):
         url = reverse('suggestion:index')
         self.assertEquals(resolve(url).func, index)
+
+    def test_add_url(self):
+        url = reverse('suggestion:add')
+        self.assertEquals(resolve(url).func, add)
