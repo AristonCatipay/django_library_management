8a8f65845c9a269f29015bc8e92a0b9871fa4188
Ariston <aristoncatipay123@gmail.com>
2023-12-02 21:38:06 +0800

    Create test for add book review page urls.

---
+++ review/tests/test_urls.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from review.views import add
+from book.models import Book
+
+class ReviewTestUrls(TestCase):
+    def setUp(self):
+        self.book = Book.objects.create(
+            title = 'Book title test',
+            isbn_number = '1234324345',
+            date_published = '2023-3-3',
+            inventory = 3, 
+            rack_number = 3,
+            rack_level_number = 3,
+        )
+
+    def test_add_url(self):
+        url = reverse('review:add', args=[self.book.pk])
+        self.assertEquals(resolve(url).func, add)
+
+    def tearDown(self):
+        self.book.delete()
