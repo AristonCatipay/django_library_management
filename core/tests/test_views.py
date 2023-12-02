1ba612ea8932acc000044425f569be9ea5877609
Ariston <aristoncatipay123@gmail.com>
2023-12-02 20:31:56 +0800

    Create test for signin view function.

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -16,6 +16,28 @@ class CoreViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response ,'core/home.html')
 
+    def test_signin_view(self):
+        url = reverse('core:signin')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response ,'core/signin.html')
+
+        data = {
+            'username': self.user.username,
+            'password': self.user.password,
+        }
+
+        response = self.client.post(url, data)
+        print("\nTest Data Used (Sign in):", data, "\n")
+
+        if response.context:
+            # Retrieve form instance to access errors
+            form = response.context['form']
+            if form.errors:
+                print(form.errors)
+
+        self.assertEqual(response.status_code, 302)
+
     def test_signup_view(self):
         url = reverse('core:signup')
         response = self.client.get(url)
