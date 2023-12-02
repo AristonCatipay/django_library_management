a6d4a31f22d9c80fa4dbcad1bc8e879a84b1d6c2
Ariston <aristoncatipay123@gmail.com>
2023-12-02 20:39:24 +0800

    Create test for index view function.

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User, Group
 from course.models import Course
+from user_profile.models import Profile
 
 class CoreViewTestCase(TestCase):
     def setUp(self):
@@ -9,12 +10,25 @@ class CoreViewTestCase(TestCase):
         self.course = Course.objects.create(name='Not specified', abbreviation='NS')
         self.group = Group.objects.create(name='student')
 
+        self.group_staff = Group.objects.create(name='staff')
+        self.course = Course.objects.get(abbreviation='NS')
+        self.user_staff = User.objects.create_user(username='testuserstaff', password='12345')
+        self.user_staff_profile = Profile.objects.create(user=self.user_staff, course=self.course)
+        self.user_staff.groups.add(self.group_staff)
+
 
     def test_home_view(self):
         url = reverse('core:home')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response ,'core/home.html')
+
+    def test_index_view(self):
+        self.client.force_login(self.user_staff)
+        url = reverse('core:index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response ,'core/index.html')
 
     def test_signin_view(self):
         url = reverse('core:signin')
@@ -75,3 +89,6 @@ class CoreViewTestCase(TestCase):
         self.user.delete()
         self.course.delete()
         self.group.delete()
+        self.user_staff_profile.delete()
+        self.user_staff.delete()
+        self.group_staff.delete()
