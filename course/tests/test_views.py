23e23074e45718335de07cb8e4898390f2ca70a1
Ariston <aristoncatipay123@gmail.com>
2023-12-03 17:41:44 +0800

    Create test for edit course view function.

--- course/tests/test_views.py
+++ course/tests/test_views.py
@@ -46,6 +46,27 @@ class CourseTestViews(TestCase):
                 print(form.errors)
         self.assertEqual(response.status_code, 302)
 
+    def test_edit_view(self):
+        self.client.force_login(self.user_staff)
+        url = reverse('course:edit', kwargs={'primary_key': self.course.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'course/form.html')
+
+        data = {
+            'name': 'This is a course',
+            'abbreviation': 'This is an abbreviation', 
+        }
+        response = self.client.post(url, data)
+        print("\nTest Data Used (Edit Course):", data, "\n")
+
+        if response.context:
+            # Retrieve form instance to access errors
+            form = response.context['form']
+            if form.errors:
+                print(form.errors)
+        self.assertEqual(response.status_code, 302)
+
     def test_delete_view(self):
         self.client.force_login(self.user_staff)
         url = reverse('course:delete', kwargs={'primary_key': self.course.pk})
