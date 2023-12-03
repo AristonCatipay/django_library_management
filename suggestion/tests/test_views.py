caa0738decc8cc754eddd3cdced2faa5c6bac118
Ariston <aristoncatipay123@gmail.com>
2023-12-03 14:59:25 +0800

    Create test for add suggestion view function.

--- suggestion/tests/test_views.py
+++ suggestion/tests/test_views.py
@@ -25,6 +25,28 @@ class SuggestionTestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'suggestion/index.html')
 
+    def test_add_views(self):
+        self.client.force_login(self.user)
+        url = reverse('suggestion:add')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'suggestion/form.html')
+
+        data = {
+            'book_title': self.suggestion.book_title,
+            'comment': self.suggestion.comment, 
+            'created_by': self.suggestion.created_by,
+        }
+        response = self.client.post(url, data)
+        print("\nTest Data Used (Add Suggestion):", data, "\n")
+
+        if response.context:
+            # Retrieve form instance to access errors
+            form = response.context['form']
+            if form.errors:
+                print(form.errors)
+        self.assertEqual(response.status_code, 302)
+
     def tearDown(self):
         self.suggestion.delete()
         self.user.delete()
