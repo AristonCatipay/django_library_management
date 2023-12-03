b67f7a27008f7845758f618aa657ea77c17c4fc9
Ariston <aristoncatipay123@gmail.com>
2023-12-03 18:17:44 +0800

    Create test for book index page urls.

---
+++ book/tests/test_urls.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from book.views import index, add, detail, edit, add_author, add_author_in_author_list
+from book.models import Book, Author, Author_List
+
+class BookTestUrls(TestCase):
+    def setUp(self):
+        self.book = Book.objects.create(
+            title = 'Book title test',
+            isbn_number = '1234324345',
+            date_published = '2023-3-3',
+            inventory = 3, 
+            rack_number = 3,
+            rack_level_number = 3,
+        )
+
+    def test_index_url(self):
+        url = reverse('book:index')
+        self.assertEquals(resolve(url).func, index)
+
+    def tearDown(self):
+        self.book.delete()
