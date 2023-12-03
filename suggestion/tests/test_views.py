8a4ef430f673d50aeb39d3487e885f6d3ac2c650
Ariston <aristoncatipay123@gmail.com>
2023-12-03 14:59:51 +0800

    Create test for edit suggestion view function.

--- suggestion/tests/test_views.py
+++ suggestion/tests/test_views.py
@@ -47,6 +47,29 @@ class SuggestionTestViews(TestCase):
                 print(form.errors)
         self.assertEqual(response.status_code, 302)
 
+
+    def test_edit_views(self):
+        self.client.force_login(self.user)
+        url = reverse('suggestion:edit', kwargs={'primary_key': self.suggestion.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'suggestion/form.html')
+
+        data = {
+            'book_title': 'This is the new book title',
+            'comment': 'This is the new book comment', 
+            'created_by': self.suggestion.created_by,
+        }
+        response = self.client.post(url, data)
+        print("\nTest Data Used (Edit Suggestion):", data, "\n")
+
+        if response.context:
+            # Retrieve form instance to access errors
+            form = response.context['form']
+            if form.errors:
+                print(form.errors)
+        self.assertEqual(response.status_code, 302)
+
     def tearDown(self):
         self.suggestion.delete()
         self.user.delete()
