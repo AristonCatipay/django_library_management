233bf8b61b6a703afd984b2821645ccd998d5592
Ariston <aristoncatipay123@gmail.com>
2023-12-03 13:31:36 +0800

    Create test for edit suggestion page urls.

--- suggestion/tests/test_urls.py
+++ suggestion/tests/test_urls.py
@@ -1,8 +1,24 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse, resolve
-from suggestion.views import index, add
+from django.contrib.auth.models import User
+from suggestion.views import index, add, edit
+from suggestion.models import Suggestion
 
-class SuggestionTestUrls(SimpleTestCase):
+class SuggestionTestUrls(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            first_name = 'firstname test',
+            last_name = 'lastname test',
+            username='testuser',
+            email='emailtest',
+            password='12345'
+        )
+        self.suggestion = Suggestion.objects.create(
+            book_title = '',
+            comment = '',
+            created_by = self.user,
+        )
+
     def test_index_url(self):
         url = reverse('suggestion:index')
         self.assertEquals(resolve(url).func, index)
@@ -10,3 +26,11 @@ class SuggestionTestUrls(SimpleTestCase):
     def test_add_url(self):
         url = reverse('suggestion:add')
         self.assertEquals(resolve(url).func, add)
+
+    def test_edit_url(self):
+        url = reverse('suggestion:edit', args=[self.suggestion.pk])
+        self.assertEquals(resolve(url).func, edit)
+
+    def tearDown(self):
+        self.suggestion.delete()
+        self.user.delete()
