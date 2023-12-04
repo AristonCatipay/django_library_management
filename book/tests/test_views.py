dd8421c9374cba33fcc8213231b99447abfba726
Ariston <aristoncatipay123@gmail.com>
2023-12-04 19:00:48 +0800

    Create test for add author in author list view function.

--- book/tests/test_views.py
+++ book/tests/test_views.py
@@ -133,6 +133,27 @@ class BookTestViews(TestCase):
                 print(form.errors)
         self.assertEqual(response.status_code, 302)
 
+    def test_add_author_in_author_list_views(self):
+        self.client.force_login(self.user_staff)
+        url = reverse('book:add_author_in_author_list', kwargs={'primary_key' : self.author.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'book/form.html')
+
+        data = {
+            'book' : self.book.pk,
+            'author' : self.author.pk,
+        }
+        response = self.client.post(url, data)
+        print("\nTest Data Used (Add Author in Author List):", data, "\n")
+
+        if response.context:
+            # Retrieve form instance to access errors
+            form = response.context['form']
+            if form.errors:
+                print(form.errors)
+        self.assertEqual(response.status_code, 302)
+
     def tearDown(self):
         self.author_list.delete()
         related_author_lists = Author_List.objects.filter(book=self.book)
