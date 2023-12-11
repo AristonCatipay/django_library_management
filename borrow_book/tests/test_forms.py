0c1d4df1da656e5b654f03c2d120d68e6c44e9e0
Ariston <aristoncatipay123@gmail.com>
2023-12-11 22:27:52 +0800

    Create test for borrow book pick up approve form.

--- borrow_book/tests/test_forms.py
+++ borrow_book/tests/test_forms.py
@@ -7,3 +7,9 @@ class BorrowBookTestForms(TestCase):
             'pick_up_date' : '2023-12-06',
         })
         self.assertTrue(form.is_valid(), form.errors.as_data())
+
+    def test_book_pick_up_approve_form(self):
+        form = BookPickUpApproveForm(data={
+            'return_due_date' : '2023-12-09',
+        })
+        self.assertTrue(form.is_valid(), form.errors.as_data())
