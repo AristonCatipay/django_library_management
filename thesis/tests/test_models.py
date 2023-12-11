e2754adf114e8012615a0826051b3859c40f8394
Ariston <aristoncatipay123@gmail.com>
2023-12-12 01:09:26 +0800

    Create test for author model last name field content.

--- thesis/tests/test_models.py
+++ thesis/tests/test_models.py
@@ -18,3 +18,7 @@ class AuthorTestModel(TestCase):
     def test_first_name_content(self):
         expected_value = f'{self.author.first_name}'
         self.assertTrue(expected_value, 'Test firstname')
+
+    def test_last_name_content(self):
+        expected_value = f'{self.author.last_name}'
+        self.assertTrue(expected_value, 'Test lastname')
