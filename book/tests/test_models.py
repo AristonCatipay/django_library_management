bd48fc2f72bb53ad48bfc25c757a92b9238804e0
Ariston <aristoncatipay123@gmail.com>
2023-12-13 19:19:43 +0800

    Create test for testing last name field in the author model.

--- book/tests/test_models.py
+++ book/tests/test_models.py
@@ -6,7 +6,6 @@ class AuthorTestModel(TestCase):
         self.author = Author.objects.create(
             first_name = 'Test firstname',
             last_name = 'Test lastname',
-            name = 'Test fullname',
         )
     def tearDown(self):
         self.author.delete()
@@ -14,3 +13,7 @@ class AuthorTestModel(TestCase):
     def test_first_name_content(self):
         expected_value = f'{self.author.first_name}'
         self.assertTrue(expected_value, 'Test firstname')
+
+    def test_last_name_content(self):
+        expected_value = f'{self.author.last_name}'
+        self.assertTrue(expected_value, 'Test lastname')
