6b2f168d41b10ef368a61d04080ad301828e68f1
Ariston <aristoncatipay123@gmail.com>
2023-12-16 16:17:50 +0800

    Only allow authenticated users to use create, read, update, delete functionality.

--- api_suggestion/views.py
+++ api_suggestion/views.py
@@ -1,22 +1,28 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.decorators import login_required
 from suggestion.models import Suggestion
-from . serializers import SuggestionSerializer
+from .serializers import SuggestionSerializer
+from rest_framework.response import Response
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def read_suggestion(request):
-    suggestion =  Suggestion.objects.all()
+    suggestion = Suggestion.objects.all()
     serializer = SuggestionSerializer(suggestion, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def create_suggestion(request):
     serializer = SuggestionSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data)
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
 
 @api_view(['PUT'])
+@permission_classes([IsAuthenticated])
 def update_suggestion(request, pk):
     try:
         suggestion = Suggestion.objects.get(pk=pk)
@@ -30,6 +36,7 @@ def update_suggestion(request, pk):
     return Response(serializer.errors, status=400)
 
 @api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
 def delete_suggestion(request, pk):
     try:
         suggestion = Suggestion.objects.get(pk=pk)
