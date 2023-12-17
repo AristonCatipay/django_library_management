54de77681f10fbff0bcda873b9fdd9416bd0981d
Ariston <aristoncatipay123@gmail.com>
2023-12-17 18:04:13 +0800

    Create endpoint for user sign-in, restrict access to unauthenticated users.

--- api_core/views.py
+++ api_core/views.py
@@ -1,14 +1,15 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth.models import User
-from core.permissions import IsStaffOrReadOnly
+from django.contrib.auth.models import User, auth
+from core.permissions import IsStaffOrReadOnly, UnauthenticatedOnly
 from .serializers import SignInSerializer, SignUpSerializer
 from course.models import Course
 from user_profile.models import Profile
 
 @api_view(['POST'])
+@permission_classes([UnauthenticatedOnly])
 def signup(request):
     signup_serializer = SignUpSerializer(data=request.data)
     
@@ -30,3 +31,19 @@ def signup(request):
             return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
     else:
         return Response(signup_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['POST'])
+@permission_classes([UnauthenticatedOnly])
+def signin(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = auth.authenticate(username=username, password=password)
+    if user is not None:
+        # User is authenticated.
+        auth.login(request, user)
+        return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+    else:
+        # Invalid credentials
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
