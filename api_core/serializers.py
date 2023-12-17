6b8a2a0ef9847e46794593d1c4fa0fd51409169d
Ariston <aristoncatipay123@gmail.com>
2023-12-17 16:08:40 +0800

    Create the Signup and Signin serializers.

---
+++ api_core/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password']
+
+class SignInSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
