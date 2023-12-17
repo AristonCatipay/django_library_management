35180a42bf669a78a552ddab82cc0ee337729aaf
Ariston <aristoncatipay123@gmail.com>
2023-12-17 18:01:24 +0800

    Create a rule that will only allow unauthenticated users to access a page.

--- core/permissions.py
+++ core/permissions.py
@@ -12,3 +12,7 @@ class IsStaffOrReadOnly(BasePermission):
         
         # Check if the user is staff or belongs to the 'staff' group for other request methods.
         return request.user.is_staff or 'staff' in [group.name for group in request.user.groups.all()]
+
+class UnauthenticatedOnly(BasePermission):
+    def has_permission(self, request, view):
+        return not request.user.is_authenticated
