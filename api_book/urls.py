5b0b6acdc1be611efda7bc40f419d42e37149433
Ariston <aristoncatipay123@gmail.com>
2023-12-18 19:45:09 +0800

    Create API endpoint for retrieving book detail with authors and reviews.

--- api_book/urls.py
+++ api_book/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('read/', views.read_book)
+    path('read/', views.read_book),
+    path('read/detail/<int:book_primary_key>/', views.read_book_detail),
 ]
--- api_book/views.py
+++ api_book/views.py
@@ -3,8 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import Q
-from book.models import Book
-from .serializers import BookSerializer
+from django.shortcuts import get_object_or_404
+from book.models import Book, Author_List
+from review.models import Review, Reviewed_Item
+from .serializers import BookSerializer, AuthorSerializer, AuthorListSerializer, ReviewedItemSerializer
 
 
 @api_view(['GET'])
@@ -18,3 +20,21 @@ def read_book(request):
 
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)    
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def read_book_detail(request, book_primary_key):
+    book = get_object_or_404(Book, pk=book_primary_key)
+    authors = Author_List.objects.filter(book_id=book_primary_key)
+    book_reviews = Reviewed_Item.objects.filter(book_id=book_primary_key)
+
+    # Serialize single objects
+    serialized_book = BookSerializer(book).data
+    serialized_authors = [AuthorListSerializer(author).data for author in authors] 
+    serialized_reviews = [ReviewedItemSerializer(review).data for review in book_reviews]
+
+    return Response({
+        'book': serialized_book,
+        'authors': serialized_authors,
+        'book_reviews': serialized_reviews,
+    })
