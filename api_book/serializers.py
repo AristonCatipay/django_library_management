b2ce64a3cd19e138b35b9a5b97493896e26bccbb
Ariston <aristoncatipay123@gmail.com>
2023-12-18 19:42:48 +0800

    Create serializers for author, author list and review model.

--- api_book/serializers.py
+++ api_book/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
-from book.models import Book
+from book.models import Book, Author, Author_List
+from review.models import Reviewed_Item
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
+        fields = '__all__'
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+class AuthorListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author_List
+        fields = '__all__'
+
+class ReviewedItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reviewed_Item
         fields = '__all__'
