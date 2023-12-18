e952b473bbe0091642bb2f91e7b144d28515c8d2
Ariston <aristoncatipay123@gmail.com>
2023-12-18 19:01:32 +0800

    Create the functionality to filter the book by name or isbn number

--- api_book/views.py
+++ api_book/views.py
@@ -2,12 +2,19 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Q
 from book.models import Book
 from .serializers import BookSerializer
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def read_book(request):
-    book = Book.objects.all()
-    serializer = BookSerializer(book, many=True)
-    return Response(serializer.data)
+    query = request.GET.get('query', '')
+    books = Book.objects.all()
+
+    if query:
+        books = books.filter(Q(title__icontains=query) | Q(isbn_number__icontains=query))
+
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)    
