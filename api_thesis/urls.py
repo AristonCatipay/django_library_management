66b441e4ba3418623e0a70ba104c13179bd87689
Ariston <aristoncatipay123@gmail.com>
2023-12-19 14:30:08 +0800

    Create an API endpoint for updating thesis data.

--- api_thesis/urls.py
+++ api_thesis/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('read/', views.read_thesis),
     path('create/', views.create_thesis),
+    path('update/<int:thesis_primary_key>/', views.update_thesis),
     path('author/read/', views.read_author),
     path('author/create/', views.create_author),
     path('authorlist/read/', views.read_author_list),
--- api_thesis/views.py
+++ api_thesis/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from core.permissions import IsStaffOrReadOnly
 from thesis.models import Thesis, Author, Author_List
 from .serializers import ThesisSerializer, AuthorSerializer, AuthorListSerializer
@@ -18,6 +19,16 @@ def read_thesis(request):
 @permission_classes([IsAuthenticated, IsStaffOrReadOnly])
 def create_thesis(request):
     serializer = ThesisSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated, IsStaffOrReadOnly])
+def update_thesis(request, thesis_primary_key):
+    thesis = get_object_or_404(Thesis, pk=thesis_primary_key)
+    serializer = ThesisSerializer(thesis, data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
