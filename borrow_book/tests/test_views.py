c886cf276bbfac7c0b4ebcf0339b658218ba5414
Ariston <aristoncatipay123@gmail.com>
2023-12-19 23:03:42 +0800

    Refactor the name of borrow book add view function to 'create_request_to_borrow_book' in the unit test to keep them from working.

--- borrow_book/tests/test_views.py
+++ borrow_book/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from django.contrib.auth.models import User, Group
-from borrow_book.views import read_borrow_book_transactions, add, borrow_request, borrow_request_approve, book_pick_up, book_pick_up_approve, book_return, book_return_approved
+from borrow_book.views import read_borrow_book_transactions, create_request_to_borrow_book, borrow_request, borrow_request_approve, book_pick_up, book_pick_up_approve, book_return, book_return_approved
 from borrow_book.models import Borrow_Book
 from book.models import Book, Author
 from course.models import Course
@@ -58,9 +58,9 @@ class BorrowBookTestView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'borrow_book/index.html')
 
-    def test_add_views(self):
+    def test_create_request_to_borrow_book_views(self):
         self.client.force_login(self.user)
-        url = reverse('borrow_book:add', kwargs={'primary_key': self.book.pk})
+        url = reverse('borrow_book:create_request_to_borrow_book', kwargs={'book_primary_key': self.book.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
 
