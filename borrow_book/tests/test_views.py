7cc662eef25644ea95978eea3e943c5ac12a2b16
Ariston <aristoncatipay123@gmail.com>
2023-12-20 18:05:17 +0800

    Fix the bug in the test files that occured when refactoring the 'book_pick_up' view funtion to 'read_books_for_pick_up'.

--- borrow_book/tests/test_views.py
+++ borrow_book/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from django.contrib.auth.models import User, Group
-from borrow_book.views import read_borrow_book_transactions, create_request_to_borrow_book, read_requests_to_borrow_book, approve_borrow_book_request, book_pick_up, book_pick_up_approve, book_return, book_return_approved
+from borrow_book.views import read_borrow_book_transactions, create_request_to_borrow_book, read_requests_to_borrow_book, approve_borrow_book_request, read_books_for_pick_up, book_pick_up_approve, book_return, book_return_approved
 from borrow_book.models import Borrow_Book
 from book.models import Book, Author
 from course.models import Course
@@ -107,9 +107,9 @@ class BorrowBookTestView(TestCase):
                 print(form.errors)
         self.assertEqual(response.status_code, 302)
 
-    def test_book_pick_up_views(self):
+    def test_read_books_for_pick_up_views(self):
         self.client.force_login(self.user_staff)
-        url = reverse('borrow_book:book_pick_up')
+        url = reverse('borrow_book:read_books_for_pick_up')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'borrow_book/book_pick_up.html')
