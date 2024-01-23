938dd9d4bb6fa562485630f58f3d85460a902cee
Ariston <aristoncatipay123@gmail.com>
2024-01-23 22:30:54 +0800

    Update the change password functionality. Check if the old password that the user give match their current password.

--- user_profile/views.py
+++ user_profile/views.py
@@ -47,16 +47,21 @@ def edit(request):
 @login_required
 def change_password(request):
     if request.method == 'POST':
+        old_password = request.POST['old_password'] 
         new_password = request.POST['new_password'] 
         confirm_new_password = request.POST['confirm_new_password'] 
 
-        if new_password == confirm_new_password:
-            request.user.set_password(new_password)
-            request.user.save()
-            messages.success(request, 'Success! The password has been edited.')
-            return redirect('core:signin')
+        if request.user.check_password(old_password):
+            if new_password == confirm_new_password:
+                request.user.set_password(new_password)
+                request.user.save()
+                messages.success(request, 'Success! The password has been edited.')
+                return redirect('core:signin')
+            else:
+                messages.error(request, 'Failed to update password. Password don\'t match')
+                return redirect('profile:change_password')
         else:
-            messages.error(request, 'Password don\'t match')
+            messages.error(request, 'Failed to update password. Old password does not match.')
             return redirect('profile:change_password')
             
     return render(request, 'user_profile/change_password.html', {
