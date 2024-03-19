5eeea9b8ef4894b42973782046240ff40e93989d
Ariston <aristoncatipay123@gmail.com>
2024-03-19 23:46:04 +0800

    Delete the api_course app.

--- django_library_management/urls.py
+++ django_library_management/urls.py
@@ -33,7 +33,6 @@ urlpatterns = [
     path('borrow-book/', include('borrow_book.urls')),
     path('review/', include('review.urls')),
     path('api/suggestion/', include('api_suggestion.urls')),
-    path('api/course/', include('api_course.urls')),
     path('api/review/', include('api_review.urls')),
     path('api/thesis/', include('api_thesis.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
