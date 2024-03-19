4936ea0a118ef0fb350ec45ab16dfdabe90e8997
Ariston <aristoncatipay123@gmail.com>
2024-03-19 23:47:05 +0800

    Delete the api_suggestion app.

--- django_library_management/urls.py
+++ django_library_management/urls.py
@@ -32,6 +32,5 @@ urlpatterns = [
     path('suggestion/', include('suggestion.urls')),
     path('borrow-book/', include('borrow_book.urls')),
     path('review/', include('review.urls')),
-    path('api/suggestion/', include('api_suggestion.urls')),
     path('api/thesis/', include('api_thesis.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
